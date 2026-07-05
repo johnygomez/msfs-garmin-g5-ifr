6829384b9a6c2aa360c9611eea3e890fad2ed299
Jan Gamec <jan@gamec.sk>
2026-07-05 22:47:32 +0200

    feat: ✨ add msfs project to the repo

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,14 +3,12 @@ import resolve from "@rollup/plugin-node-resolve"
 import { copyFileSync } from "node:fs"
 import { resolve as pathResolve } from "node:path"
 
-const sdkProject =
-    "/mnt/c/Users/janga/Documents/msfs/projects/garmin-g5-enhanced/PackageSources"
-const htmlUiPath = "html_ui/Pages/VCockpit/Instruments/NavSystems/Enhanced-AS5"
+const outDir = "garmin-g5-enhanced/PackageSources/html_ui/Pages/VCockpit/Instruments/NavSystems/Enhanced-AS5"
 
 export default {
     input: "src/instruments/Enhanced-AS5/index.ts",
     output: {
-        file: pathResolve(sdkProject, htmlUiPath, "AS5.js"),
+        file: pathResolve(outDir, "AS5.js"),
         format: "iife",
         name: "garminG5",
         globals: {
@@ -27,7 +25,7 @@ export default {
             name: "copy-css",
             writeBundle() {
                 const src = "src/instruments/Enhanced-AS5/style.css"
-                const dest = pathResolve(sdkProject, htmlUiPath, "AS5.css")
+                const dest = pathResolve(outDir, "AS5.css")
                 copyFileSync(src, dest)
             },
         },
