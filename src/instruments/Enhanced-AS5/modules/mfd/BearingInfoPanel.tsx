fca9ebab489f864a4d5c416abead061622d2abc9
Jan Gamec <jan@gamec.sk>
2026-07-29 11:17:42 +0200

    style: 💄 make the bearing info panels smaller, so that VDI fits

--- src/instruments/Enhanced-AS5/modules/mfd/BearingInfoPanel.tsx
+++ src/instruments/Enhanced-AS5/modules/mfd/BearingInfoPanel.tsx
@@ -19,16 +19,18 @@ export interface BearingInfoPanelProps extends ComponentProps {
 }
 
 abstract class BearingInfoPanel extends ReactiveComponent<BearingInfoPanelProps> {
-    readonly nav = this.props.bus.getSubscriber<G5NavEvents>()
+    protected readonly nav = this.props.bus.getSubscriber<G5NavEvents>()
 
-    readonly nav1HasLoc = this.consume(this.nav.on('nav1_has_loc'), false)
-    readonly nav2HasLoc = this.consume(this.nav.on('nav2_has_loc'), false)
+    protected readonly nav1HasLoc = this.consume(this.nav.on('nav1_has_loc'), false)
+    protected readonly nav2HasLoc = this.consume(this.nav.on('nav2_has_loc'), false)
 
-    readonly source = this.track(this.props.state.map(state => state.source))
-    readonly style = this.track(displayStyle(this.props.state.map(state => state.visible)))
+    protected readonly source = this.track(this.props.state.map(state => state.source))
+    protected readonly style = this.track(
+        displayStyle(this.props.state.map(state => state.visible))
+    )
 
     // A bearing pointer is never TACAN-driven, so the label resolves to VOR/LOC or ADF.
-    readonly sourceLabel = this.track(
+    protected readonly sourceLabel = this.track(
         MappedSubject.create(
             ([source, hasLoc1, hasLoc2]) => resolveNavSourceLabel(source, false, hasLoc1, hasLoc2),
             this.source,
@@ -42,9 +44,9 @@ export class LeftBearingInfoPanel extends BearingInfoPanel {
     render(): VNode {
         return (
             <div id="LeftBearingInfoPanel" style={this.style}>
-                <svg class="LeftBearingInfo-shape" viewBox="0 0 120 60">
+                <svg class="LeftBearingInfo-shape" viewBox="0 0 120 50">
                     <path
-                        d="M 1 1 H 85 Q 98 30 119 45 V 59 H 1 Z"
+                        d="M 1 1 H 85 Q 98 25 119 38 V 49 H 1 Z"
                         fill={Colors.BLACK}
                         stroke={Colors.LIGHT_GREY}
                         stroke-width="1.5"
@@ -76,11 +78,11 @@ export class RightBearingInfoPanel extends BearingInfoPanel {
     render(): VNode {
         return (
             <div id="RightBearingInfoPanel" style={this.style}>
-                <svg class="RightBearingInfo-shape" viewBox="0 0 120 60">
+                <svg class="RightBearingInfo-shape" viewBox="0 0 120 50">
                     <path
-                        d="M 1 45 Q 22 30 35 1 H 119 V 59 H 1 Z"
-                        fill="#000000"
-                        stroke="var(--light-grey)"
+                        d="M 1 38 Q 22 25 35 1 H 119 V 49 H 1 Z"
+                        fill={Colors.BLACK}
+                        stroke={Colors.LIGHT_GREY}
                         stroke-width="1.5"
                     />
                 </svg>
